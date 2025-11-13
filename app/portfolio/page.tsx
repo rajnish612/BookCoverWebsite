20633a7171820b2f66b62eb0c342280a98dedea5
Rajnish Nath <rnish612@gmail.com>
2025-11-13 11:27:30 +0530

    fixed module error

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import HeroSection from "../components/Portfolio/HeroSection";
 import BestCollections from "../components/Portfolio/BestCollections";
 import AboutMeCard from "../components/AboutMeCard";
-import RecentWorks from "../components/Portfolio/RecentWorks";
 import WhatCanIDo from "../components/Portfolio/About/WhatCanIDo";
 
 const page = () => {
